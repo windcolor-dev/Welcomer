da385246509acf034aafa3558bbe945a11ddbd50
windcolor <teaglehao@gmail.com>
2021-05-25 16:42:08 -0700

    Fixed issues and cmd sending

--- src/main/java/com/windpvp/welcomer/JoinListener.java
+++ src/main/java/com/windpvp/welcomer/JoinListener.java
@@ -1,46 +1,65 @@
 package com.windpvp.welcomer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class JoinListener implements Listener {
 
 	private JavaPlugin plugin;
-	private FileConfiguration config = plugin.getServer().getPluginManager().getPlugin("Welcomer").getConfig();
+	private FileConfiguration config;
+	private String cmdData;
+	private String replacedCmdData;
 
 	public JoinListener(JavaPlugin plugin) {
 		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		config = plugin.getConfig();
 	}
 
 	@EventHandler
-	public void on(PlayerJoinEvent event) {
+	public void onJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		if (config.getBoolean("message.enabled")) {
 			for (int count = 1; count > 0; count++) {
 				String messageData = config.getString("message.line" + count);
 				if (messageData == null) {
-					break; 
+					break;
 				}
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', messageData));
+				new BukkitRunnable() {
+
+					@Override
+					public void run() {
+						player.sendMessage(ChatColor.translateAlternateColorCodes('&', messageData));
+					}
+				}.runTaskLater(plugin, 10);
 			}
 		}
-		if (config.getBoolean("console_command")) {
+		if (config.getBoolean("console_command.enabled")) {
 			for (int count = 1; count > 0; count++) {
-				String cmdData = config.getString("console_command.cmd" + count);
+				cmdData = config.getString("console_command.cmd" + count);
 				if (cmdData == null) {
 					break;
 				}
 				if (cmdData.contains("PLAYER")) {
-					cmdData = cmdData.replace("PLAYER", "");
+					replacedCmdData = cmdData.replace("PLAYER", player.getDisplayName());
 				}
-				plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), cmdData);
+
+				new BukkitRunnable() {
+
+					@Override
+					public void run() {
+						Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), replacedCmdData);
+					}
+					
+				}.runTaskLater(plugin, 10);
 			}
 		}
 	}
